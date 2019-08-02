4676a741b724d99c557825815c1cb172372d46a5
Camila Almeida <camila.almeida.br@hotmail.com>
2019-08-02 16:09:55 -0300

    Criar form Cadastro Empresa

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Candidato),
+admin.site.register(models.Empresa)
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from website.models import Empresa, Candidato
 
 # Create your views here.
 
@@ -10,6 +11,15 @@ def login(request):
     return render(request, 'login.html')
 
 def cadastro_empresa(request):
+    if request.method == 'POST':
+        empresa = Empresa()
+        empresa.razao_social = request.POST.get('razao_social')
+        empresa.cnpj = request.POST.get('cnpj')
+        empresa.senha = request.POST.get('senha')
+        empresa.save()
+        print("empresa: ", empresa)
+        return render(request, 'login.html')
+
     return render(request, 'cadastro_empresa.html')
 
 def cadastro_candidato(request):
