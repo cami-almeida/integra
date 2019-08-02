cc93aeed9731ceb57afe95f38c2a5273deabbfad
Camila Almeida <camila.almeida.br@hotmail.com>
2019-08-02 15:35:45 -0300

    Criar models Candidato e Empresa

---
+++ website/models.py
@@ -0,0 +1,116 @@
+from django.db import models
+
+# Create your models here.
+
+class Candidato(models.Model):
+
+    GENEROS = (
+        ('F','Feminino'),
+        ('M','Masculino'),
+        ('O','Outro'),
+    )
+
+    AREAS = (
+        ('Comunicação', 'Comunicação'),
+        ('Saúde', 'Saúde'),
+        ('Tecnologia', 'Tecnologia'),
+        ('Financeiro', 'Financeiro'),
+        ('Administração', 'Administração'),
+        ('RH', 'RH'),
+        ('Jurídica', 'Jurídica'),
+        ('Outro', 'Outro')
+    )
+
+    nome = models.CharField(
+        max_length=255,
+        verbose_name='Nome'
+    )
+
+    sobrenome = models.CharField(
+        max_length=255,
+        verbose_name='Sobrenome'
+    )
+
+    rne = models.CharField(
+        max_length=255,
+        verbose_name='Rne'
+    )
+
+    pais_de_origem = models.CharField(
+        max_length=255,
+        verbose_name='País de Origem'
+    )
+
+    data_nascimento = models.DateField()
+
+    genero = models.CharField(
+        max_length=255,
+        verbose_name='Gênero',
+        choices = GENEROS
+    )
+
+    senha = models.CharField(
+        max_length=255,
+        verbose_name='Senha'
+    )
+
+    email = models.EmailField(
+        max_length=255,
+        verbose_name='E-mail'
+    )
+
+    telefone = models.CharField(
+        max_length=10,
+        verbose_name='Telefone'
+    )
+
+    celular = models.CharField(
+        max_length=11,
+        verbose_name='Celular'
+    )
+
+    apresentacao = models.TextField(
+        null=True,
+        blank=True
+    )
+
+    area = models.CharField(
+        verbose_name = 'Áreas',
+        choices = AREAS,
+        max_length=255
+    )
+
+    area_outros = models.CharField(
+        null= True,
+        blank=True,
+        max_length=22,
+        verbose_name = 'Caso outra, qual?'
+    )
+
+    data_de_criacao = models.DateField(auto_now=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome + '' + self.sobrenome
+
+class Empresa(models.Model):
+    razao_social = models.CharField(
+        max_length = 255,
+        verbose_name = 'Razão Social'
+    )
+
+    cnpj = models.CharField(
+        max_length = 14,
+        verbose_name = 'CNPJ'
+    )
+
+    senha = models.CharField(
+        max_length = 255,
+        verbose_name = 'Senha'
+    )
+
+    data_de_criacao = models.DateField(auto_now=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.razao_social
