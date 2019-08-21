3d810882844c20c34190306ea18f16d18ac1d8d7
Camila Almeida <camila.almeida.br@hotmail.com>
2019-08-20 22:02:50 -0300

    Atualizar configuraçao heroku

--- integra/settings.py
+++ integra/settings.py
@@ -115,7 +115,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-django_heroku.setting(locals())
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
